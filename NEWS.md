29496c17a476bf05caa1d8815e9a24fb4dca409a
Jack Wasey <jack@jackwasey.com>
2020-05-26 11:39:44 -0400

    Initial commit

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# fastcats 0.0.0.9000
+
+* Added a `NEWS.md` file to track changes to the package.
---
+++ R/fastcats-package.R
@@ -0,0 +1,7 @@
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+#' @import RcppEigen
+#' @useDynLib fastcats, .registration = TRUE
+## usethis namespace: end
+NULL
+
